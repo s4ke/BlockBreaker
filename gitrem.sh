c5412c7b2fe0be1e6f58779dcd0484480ad617e4
s4ke <braun5@gmx.de>
2012-01-02 14:16:34 +0100

    today is not my day... indentations...

--- gitrem.sh
+++ gitrem.sh
@@ -8,21 +8,21 @@ et -o errexit
 
 if [ $# -eq 0 ]; then
         exit 0
-    fi
+fi
 
-    # make sure we're at the root of git repo
-    if [ ! -d .git ]; then
-            echo "Error: must run this script from the root of a git repository"
-                exit 1
-            fi
+# make sure we're at the root of git repo
+if [ ! -d .git ]; then
+     echo "Error: must run this script from the root of a git repository"
+     exit 1
+fi
 
-            # remove all paths passed as arguments from the history of the repo
-            files=$@
+# remove all paths passed as arguments from the history of the repo
+files=$@
 
-            # you could add <introduction-revision-sha1>..HEAD here to limit to the number of commits
-            # see http://stackoverflow.com/questions/872565/how-do-i-remove-sensitive-files-from-gits-history
-            git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch $files" HEAD
+# you could add <introduction-revision-sha1>..HEAD here to limit to the number of commits
+# see http://stackoverflow.com/questions/872565/how-do-i-remove-sensitive-files-from-gits-history
+git filter-branch --index-filter "git rm -rf --cached --ignore-unmatch $files" HEAD
 
-            # remove the temporary history git-filter-branch otherwise leaves behind for a long time
-            rm -rf .git/refs/original/ && git reflog expire --all &&  git gc --aggressive --prune
+# remove the temporary history git-filter-branch otherwise leaves behind for a long time
+rm -rf .git/refs/original/ && git reflog expire --all &&  git gc --aggressive --prune
 
